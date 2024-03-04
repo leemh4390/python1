38608a8fe3426bbee22b156d58d9bbc5688df422
leemh4390 <leemh44390@gmail.com>
2024-03-04 18:30:36 +0900

    2024/03/04

--- repos_python/randombox.py
+++ repos_python/randombox.py
@@ -4,19 +4,19 @@ x = 3000
 total_value = 0
 
 def open_box() :
-    probabilities = [0.0009, 0.1, 2.0, 18.6254, 28.9746, 50.2991]
+    probabilities = [0.00064, 0.099, 1.062, 58.83118, 29.993, 10.01418]
 
     if sum(probabilities) == 100 :
         items       = ['Grand X', 'Grand A', 'Grand B', 'Grand C', 'Grand D', 'Grand E']
         selected_item   = random.choices(items, weights=probabilities, k=1)[0]
         return selected_item
 
-item_info = {  'Grand X': {'count': 0,  'price': 10000000}
-             , 'Grand A': {'count': 0,  'price': 1000000}
-             , 'Grand B': {'count': 0,  'price': 100000}
-             , 'Grand C': {'count': 0,  'price': 20000}
-             , 'Grand D': {'count': 0,  'price': 15000}
-             , 'Grand E': {'count': 0,  'price': 10000}}
+item_info = {  'Grand X': {'count': 0,  'price': 86000000}
+             , 'Grand A': {'count': 0,  'price': 8600000}
+             , 'Grand B': {'count': 0,  'price': 1700000}
+             , 'Grand C': {'count': 0,  'price': 167000}
+             , 'Grand D': {'count': 0,  'price': 120000}
+             , 'Grand E': {'count': 0,  'price': 105000}}
 
 for i in range(x):
     selected_item = open_box()
@@ -27,7 +27,7 @@ for item, info in item_info.items():
     price = info['price']
     total_price = count * price
     total_value += total_price
-    print(f"{item} 는 {count} 회 당첨되었습니다.")
-    print(f"총 당첨 금액은 {price * count} 원 입니다.")
+    #print(f"{item} 는 {count} 회 당첨되었습니다.")
+    #print(f"총 당첨 금액은 {price * count} 원 입니다.")
 
-print(f"Total Value: {total_value}")
+print(f"총 이익금 : {x * 100000} 총계: {total_value}")
